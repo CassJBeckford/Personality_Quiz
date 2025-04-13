4217f3a2715bda8b7754a06b7ce122031a0a5d17
cassjbeckford <cassjbeckford282@outlook.com>
2025-04-13 18:40:48 +0100

    add an array with questions, keywords, and elements

--- quiz-project2/src/App.jsx
+++ quiz-project2/src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const questions = [
+    {
+      question: "What's your favorite color?",
+      options: ["Red 🔴", "Blue 🔵", "Green 🟢", "Yellow 🟡"],
+    },
+  ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const keywords = {
+    Fire: "fire",
+    Water: "water",
+    Earth: "earth",
+    Air: "air",
+  };
+
+  const elements = {
+    "Red 🔴": "Fire",
+    "Blue 🔵": "Water",
+    "Green 🟢": "Earth",
+    "Yellow 🟡": "Air",
+  };
+
+  return ()
 }
 
 export default App
